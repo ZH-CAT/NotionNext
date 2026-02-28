c346965ff303a1ddcf772df31df254a0cc74f35e
ZH-CAT <71161357+ZH-CAT@users.noreply.github.com>
2026-02-28 20:56:39 +0800

    Update config.js

--- themes/endspace/config.js
+++ themes/endspace/config.js
@@ -46,7 +46,7 @@ const CONFIG = {
   ENDSPACE_LOADING_TEXT_FADEOUT: 'WELCOME', // 淡出阶段显示的文字
   
   // 加载动画中的图片，可以使用public目录下的图片如'/images/logo.png'，也可以使用url如https://example.com/image.png
-  ENDSPACE_LOADING_IMAGE: https://s41.ax1x.com/2026/02/28/peSKWd0.jpg, // 加载动画中显示的图片
+  ENDSPACE_LOADING_IMAGE: 'https://s41.ax1x.com/2026/02/28/peSKWd0.jpg', // 加载动画中显示的图片
 
   // ============================================
   // 水印文字配置 (Watermark Text)
